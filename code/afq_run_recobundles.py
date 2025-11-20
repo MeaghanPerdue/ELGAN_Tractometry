e5ef3028761751347dcf1ee67d38d7ef3634da10
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2025-11-20 16:34:05 -0500

    prior version did not actually call recobundles segmentation, tweaked the initialization

--- code/afq_run_recobundles.py
+++ code/afq_run_recobundles.py
@@ -4,20 +4,25 @@
 from AFQ.api.group import GroupAFQ
 
 # Initialize AFQ object
+
 afq = GroupAFQ(
     bids_path="/bids",
     output_dir="/derivatives/afq",
-    segmentation_params={'seg_algo': 'reco'},     # switch to RecoBundles
-    refine_reco=True,               # enable refinement
-    rb_recognize_params={
-        "model_clust_thr": 1.25,
-        "pruning_thr": 12,
-        "reduction_thr": 25
+    segmentation_params={
+        "seg_algo": "reco",  # Use RecoBundles
+        "refine_reco": True, # Optional refinement
+        "rb_recognize_params": {
+            "model_clust_thr": 1.25,
+            "pruning_thr": 12,
+            "reduction_thr": 25
+        }
     }
 )
 
+
 # Remove old segmentation outputs only
 afq.clobber(dependent_on="recog")
+
 
 # Recompute segmentation and subsequent steps
 afq.export_all()
