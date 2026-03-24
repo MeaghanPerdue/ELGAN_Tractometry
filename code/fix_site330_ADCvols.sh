194790e0d0cef68c6837b61428f73aa58b34d6f3
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2026-03-24 14:50:35 -0400

    inspection and cleanup

--- code/fix_site330_ADCvols.sh
+++ code/fix_site330_ADCvols.sh
@@ -2,6 +2,9 @@
 # These MRTrix3 functions were used to remove ADC maps from dwi 4D .nii.gz images that were incorrectly converted by dcm2niix, then correct the bval and bvec files.
 # Runs rough tractography for checking accuracy of gradient files, the .tck files should be deleted after visually inspecting in mrview
 # After visually inspecting output dwi and .tck files, remove the original converted files and rename the dwi.nii.gz, dwi.bvec, and dwi.bval files according to BIDS
+# to visually inspect mask and tractogram in mrtrix, navigate to subject's dwi folder and run:
+# mrview mask.nii.gz --tractography.load wb_tracts_10k.tck
+# imperfect mask ok/expected due to lack of preprocessing, as long as major streamlines seem good
 
 # See data/site-330/README for affected subjects
 # run this as ./fix_site330_ADCvols.sh [subject-id]
@@ -31,4 +34,12 @@ echo "create rough tractogram for visual inspection"
 tckgen -algorithm tensor_det dwi.nii.gz -fslgrad dwi_gradcheck.bvec dwi_gradcheck.bval -mask mask.nii.gz -seed_image mask.nii.gz -select 10k wb_tracts_10k.tck
 
 echo "check output tractogram, rename dwi files and delete extra files" 
+mrview mask.nii.gz --tractography.load wb_tracts_10k.tck
 
+### Make sure the tracts look good before running these!
+#dwi.nii.gz ${1}_ses-03_dwi.nii.gz
+#dwi_gradcheck.bvec ${1}_ses-03_dwi.bvec
+#dwi_gradcheck.bval ${1}_ses-03_dwi.bval
+#rm mask.nii.gz
+#rm wb_tracts_10k.tck
+#rm dwi*
