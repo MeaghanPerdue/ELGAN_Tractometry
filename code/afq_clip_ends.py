c17989e937608a90df8405d8049341a2dec1849c
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2026-04-02 13:46:13 -0400

    API script to rerun tract segmentation with end clipping

--- code/afq_clip_ends.py
+++ code/afq_clip_ends.py
@@ -1,23 +1,26 @@
 # This script removes all outputs from bundle recognition on and re-runs segmentation and tract profiling with end-clipping ON
 # use this in order to do end-clipping using existing tractography
-
+import os
+import os.path as op
 from AFQ.api.group import GroupAFQ
 import AFQ.api.bundle_dict as abd
+import bids
+from bids.layout import BIDSLayout
+
 
 # Initialize AFQ object
 
 afq = GroupAFQ(
     bids_path="/bids",
+    preproc_pipeline='qsiprep',
     output_dir="/derivatives/afq",
     segmentation_params= dict(
         clip_edges = True
     )
 )
 
-
 # Remove old segmentation outputs only
 afq.cmd_outputs(dependent_on="recog")
-
 
 # Recompute segmentation and subsequent steps
 afq.export_all()
